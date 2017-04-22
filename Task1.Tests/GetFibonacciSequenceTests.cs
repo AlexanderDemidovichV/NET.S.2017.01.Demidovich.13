ea454d65142d9037d67a209c3a3c2407747d698a
AlexanderDemidovichV <alex.dem.v@gmail.com>
2017-04-22 23:15:37 +0300

    Fibonacci Number done

---
+++ Task1.Tests/GetFibonacciSequenceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class GetFibonacciSequenceTests
+    {
+        [Test]
+        public static void GetFibonacciSequence()
+        {
+            var expected = new List<long>() {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+            var actual = new List<long>();
+            int count = 0;
+            foreach (var number in FibonacciNumber.GetFibonacciSequence())
+            {
+                actual.Add(number);
+                if (++count == 10)
+                    break;
+            }
+            Assert.AreEqual(expected,actual);
+        }
+
+    }
+}
---
+++ Task1/FibonacciNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class FibonacciNumber
+    {
+        public static IEnumerable<long> GetFibonacciSequence()
+        {
+            yield return 1;
+            yield return 1;
+            long a = 1;
+            long b = 1;
+            while (true)
+            {
+                long c;
+
+                try
+                {
+                    c = unchecked(a + b);
+                }
+                catch (OverflowException ex)
+                {
+                    yield break;
+                }
+                a = b;
+                b = c;
+                yield return c;
+            }
+        }
+    }
+}
